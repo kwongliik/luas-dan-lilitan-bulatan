66fa00511fdcb01b971a0bc11fd65bd525bce36c
kwongliik <46083661+kwongliik@users.noreply.github.com>
2024-04-14 17:31:05 +0800

    Create luas_dan_lilitan_bulatan.py

---
+++ luas_dan_lilitan_bulatan.py
@@ -0,0 +1,9 @@
+import math
+
+jejari = float(input("Masukkan jejari bulatan (cm): "))
+
+luas = math.pi * jejari * jejari
+lilitan = 2 * math.pi * jejari
+
+print(f"Luas bulatan ialah {luas:.3f} cm2)
+print(f"Ukuran lilitan ialah {lilitan:.3f} cm)
