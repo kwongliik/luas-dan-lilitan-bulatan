7299d902c633fd0c515a20b39f0d1059803cada1
kwongliik <46083661+kwongliik@users.noreply.github.com>
2024-04-16 07:26:29 +0800

    Update luas_dan_lilitan_bulatan.py tambah ralat

--- luas_dan_lilitan_bulatan.py
+++ luas_dan_lilitan_bulatan.py
@@ -1,12 +1,12 @@
 import math
 
 def main2():
-    jejari = float(input("Masukkan jejari bulatan (cm): "))
+    jejari = float(input("Masukkan jejari bulatan (cm): ")
 
     luas = ...................
     lilitan = ..................
 
-    print(f"Luas bulatan ialah {luas:.3f} dan ukuran lilitan ialah {lilitan:.3f}")
+    print(f"Luas bulatan ialah {lus:.3f} dan ukuran lilitan ialah {lilitan:.3f")
 
 # JANGAN ubah kod di bawah
 if __name__ == "__main__":
